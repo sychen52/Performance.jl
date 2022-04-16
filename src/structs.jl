11a305dff86f017d3ebe6937506671b7fd2f1aa5
sychen52 <sychen52@gmail.com>
2022-04-16 15:54:42 -0700

    In the previous version, the where is actually not for struct
    which is not what I want to do

--- src/structs.jl
+++ src/structs.jl
@@ -1,17 +1,22 @@
 export DefaultArray
 
 # To solve this problem, you'll have to improve the `struct` definition and modify/add constructors
-struct DefaultArray{A,N,T} <: AbstractArray{T,N} where {A<:AbstractArray{T,N}}
+struct DefaultArray{T,N,A<:AbstractArray{T,N}} <: AbstractArray{T,N}
     parentarray::A
     defaultvalue::T
 end
 function DefaultArray(parentarray::A, defaultvalue::T) where {A,T}
     Tp = promote_type(eltype(A), T)
-    pa = convert.(Tp, parentarray)
-    DefaultArray{typeof(pa),ndims(A),Tp}(pa, defaultvalue)
+    if Tp === eltype(A)
+        pa = parentarray
+    else
+        pa = similar(parentarray, Tp)
+        copy!(pa, parentarray)
+    end
+    DefaultArray{Tp,ndims(A),typeof(pa)}(pa, defaultvalue)
 end
 
-function Base.getindex(a::DefaultArray{A,N,T}, i::Vararg{Int,N}) where {A,N,T}
+function Base.getindex(a::DefaultArray{T,N,A}, i::Vararg{Int,N}) where {T,N,A}
     checkbounds(Bool, a, i...) ? a.parentarray[i...] : a.defaultvalue
 end
 
